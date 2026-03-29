bdf89e0db3ebf7804c6d16edff895e19afd2978b
howie0721 <howie0721@users.noreply.github.com>
2026-03-29 03:05:38 +0000

    deploy: 1ef6e04368babc67afd6695fcbabf4adb1d69210

--- jmeter-tests/stability/ollama_llama3_long_context_stability/content/js/dashboard.js
+++ jmeter-tests/stability/ollama_llama3_long_context_stability/content/js/dashboard.js
@@ -187,7 +187,7 @@ $(document).ready(function() {
     }, [[0, 0]], 3);
 
     // Create statistics table
-    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 1, 1, 100.0, 33.0, 33, 33, 33.0, 33.0, 33.0, 33.0, 30.303030303030305, 4.912405303030303, 8.759469696969697], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["Ollama llama3 Multi-turn", 1, 1, 100.0, 33.0, 33, 33, 33.0, 33.0, 33.0, 33.0, 30.303030303030305, 4.912405303030303, 8.759469696969697], "isController": false}]}, function(index, item){
+    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 1, 1, 100.0, 28.0, 28, 28, 28.0, 28.0, 28.0, 28.0, 35.714285714285715, 5.789620535714286, 10.323660714285714], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["Ollama llama3 Multi-turn", 1, 1, 100.0, 28.0, 28, 28, 28.0, 28.0, 28.0, 28.0, 35.714285714285715, 5.789620535714286, 10.323660714285714], "isController": false}]}, function(index, item){
         switch(index){
             // Errors pct
             case 3:
