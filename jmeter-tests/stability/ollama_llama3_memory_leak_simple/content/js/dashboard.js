1acb09e8cd12987cbff2a5d8c5444f25c9d043db
howie0721 <howie0721@users.noreply.github.com>
2026-04-26 03:06:50 +0000

    deploy: 1ef6e04368babc67afd6695fcbabf4adb1d69210

--- jmeter-tests/stability/ollama_llama3_memory_leak_simple/content/js/dashboard.js
+++ jmeter-tests/stability/ollama_llama3_memory_leak_simple/content/js/dashboard.js
@@ -187,7 +187,7 @@ $(document).ready(function() {
     }, [[0, 0]], 3);
 
     // Create statistics table
-    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 1, 1, 100.0, 18.0, 18, 18, 18.0, 18.0, 18.0, 18.0, 55.55555555555555, 9.00607638888889, 13.671875000000002], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["Ollama llama3 Memory Leak", 1, 1, 100.0, 18.0, 18, 18, 18.0, 18.0, 18.0, 18.0, 55.55555555555555, 9.00607638888889, 13.671875000000002], "isController": false}]}, function(index, item){
+    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 1, 1, 100.0, 24.0, 24, 24, 24.0, 24.0, 24.0, 24.0, 41.666666666666664, 6.754557291666667, 10.25390625], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["Ollama llama3 Memory Leak", 1, 1, 100.0, 24.0, 24, 24, 24.0, 24.0, 24.0, 24.0, 41.666666666666664, 6.754557291666667, 10.25390625], "isController": false}]}, function(index, item){
         switch(index){
             // Errors pct
             case 3:
