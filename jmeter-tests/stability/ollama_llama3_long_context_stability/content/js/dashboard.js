7f79fce9e1b7ecd801fd2252ecee3659321198af
howie0721 <howie0721@users.noreply.github.com>
2026-07-19 03:05:08 +0000

    deploy: 1ef6e04368babc67afd6695fcbabf4adb1d69210

--- jmeter-tests/stability/ollama_llama3_long_context_stability/content/js/dashboard.js
+++ jmeter-tests/stability/ollama_llama3_long_context_stability/content/js/dashboard.js
@@ -187,7 +187,7 @@ $(document).ready(function() {
     }, [[0, 0]], 3);
 
     // Create statistics table
-    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 1, 1, 100.0, 40.0, 40, 40, 40.0, 40.0, 40.0, 40.0, 25.0, 4.052734375, 7.2265625], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["Ollama llama3 Multi-turn", 1, 1, 100.0, 40.0, 40, 40, 40.0, 40.0, 40.0, 40.0, 25.0, 4.052734375, 7.2265625], "isController": false}]}, function(index, item){
+    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 1, 1, 100.0, 22.0, 22, 22, 22.0, 22.0, 22.0, 22.0, 45.45454545454545, 7.368607954545455, 13.139204545454547], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["Ollama llama3 Multi-turn", 1, 1, 100.0, 22.0, 22, 22, 22.0, 22.0, 22.0, 22.0, 45.45454545454545, 7.368607954545455, 13.139204545454547], "isController": false}]}, function(index, item){
         switch(index){
             // Errors pct
             case 3:
--- jmeter-tests/stability/ollama_llama3_restart_recovery/content/js/dashboard.js
+++ jmeter-tests/stability/ollama_llama3_restart_recovery/content/js/dashboard.js
@@ -187,7 +187,7 @@ $(document).ready(function() {
     }, [[0, 0]], 3);
 
     // Create statistics table
-    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 1, 1, 100.0, 23.0, 23, 23, 23.0, 23.0, 23.0, 23.0, 43.47826086956522, 7.048233695652174, 11.591372282608695], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["Ollama llama3 Restart Recovery", 1, 1, 100.0, 23.0, 23, 23, 23.0, 23.0, 23.0, 23.0, 43.47826086956522, 7.048233695652174, 11.591372282608695], "isController": false}]}, function(index, item){
+    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 1, 1, 100.0, 25.0, 25, 25, 25.0, 25.0, 25.0, 25.0, 40.0, 6.484375, 10.6640625], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["Ollama llama3 Restart Recovery", 1, 1, 100.0, 25.0, 25, 25, 25.0, 25.0, 25.0, 25.0, 40.0, 6.484375, 10.6640625], "isController": false}]}, function(index, item){
         switch(index){
             // Errors pct
             case 3:
